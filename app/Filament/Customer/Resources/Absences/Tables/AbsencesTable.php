4d23f519ad67449f1c86cc8dd6b78b4ae4ab2add
ArthurSirMh <mahmoodimahdi588@gmail.com>
2026-02-12 18:30:35 +0330

    add button action for send absence message

--- app/Filament/Customer/Resources/Absences/Tables/AbsencesTable.php
+++ app/Filament/Customer/Resources/Absences/Tables/AbsencesTable.php
@@ -6,6 +6,7 @@ use Filament\Actions\Action;
 use Filament\Actions\BulkActionGroup;
 use Filament\Actions\DeleteBulkAction;
 use Filament\Actions\EditAction;
+use Filament\Notifications\Notification;
 use Filament\Tables\Columns\TextColumn;
 use Filament\Tables\Table;
 use Morilog\Jalali\Jalalian;
@@ -33,11 +34,23 @@ class AbsencesTable
                         \Log::info('دکمه بالای جدول کلیک شد');
 
                     }),
-            ])
+            ])->actions([
+                    Action::make('sendMessage')
+                        ->label('ارسال پیام')
+                        ->icon('heroicon-o-paper-airplane')
+                        ->color('success')
+                        ->action(function ($record) {
 
-            ->recordActions([
-                EditAction::make(),
-            ])
+                            // اینجا کد ارسال پیام برای همون رکورد اجرا میشه
+                            // مثال:
+                            \Log::info('ارسال پیام برای: ' . $record->id);
+
+                            Notification::make()
+                                ->title('پیام برای این دانش‌آموز ارسال شد')
+                                ->success()
+                                ->send();
+                        }),
+                ])
             ->toolbarActions([
                 BulkActionGroup::make([
                     DeleteBulkAction::make(),
